62b30672dad6c40f653cecb7bad561b201f5a782
Ceaira Frasher <95832624+ceairaF@users.noreply.github.com>
2022-02-18 15:31:05 -0500

    click events

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const overview= document.querySelector(".overview");
 const username= "ceairaF";
 const displayRepos=document.querySelector(".repo-list");
+const repoClass= document.querySelector(".repos");
+const repoData= document.querySelector(".repo-data");
 
 const getUserInfo= async function(){
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -42,4 +44,41 @@ const displayRepoInfo= function(repos){
         repoItem.innerHTML= `<h3> ${repo.name}</h3>`;
         displayRepos.append(repoItem);
     }
+};
+
+displayRepos.addEventListener("click", function(e){
+    if (e.target.matches("h3")){
+    const repoName= e.target.innerText;
+    getRepos(repoName);
+    specificRepoInfo(repoName);
+}
+}); 
+
+const specificRepoInfo= async function(repoName){
+    const requestInfo= await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await requestInfo.json();
+    //console.log(repoInfo);
+    const fetchLanguages= await fetch(repoInfo.languages_url);
+    languageData= await fetchLanguages.json();
+
+    const languages=[];
+for(const language in languageData){
+    languages.push(language);
+
+}
+    displaySpecificRepoInfo(repoInfo,languages);
+};
+
+const displaySpecificRepoInfo= function(repoInfo,languages){
+    repoData.innerHTML="";
+    repoData.classList.remove("hide");
+    repoClass.classList.add("hide");
+    const div = document.createElement("div");
+    div.innerHTML=`
+    <h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    repoData.append(div);
 };
